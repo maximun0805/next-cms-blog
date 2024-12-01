3ce39f9888eacd3931484ffdcb2c72e2e0125477
Max <5008@generative.xyz>
2024-12-01 23:05:14 +0700

    update

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Provider>
-          <CmsNavbar />
+          {/*<CmsNavbar />*/}
           {children}
         </Provider>
       </body>
